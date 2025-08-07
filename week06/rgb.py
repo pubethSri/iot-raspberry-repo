e8ccb5f6e69f2c927e13181d1ac85cd5ea4f59e7
pubethSri <66070158@kmitl.ac.th>
2025-08-07 14:43:54 +0700

    add rgb.py

---
+++ week06/rgb.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+RED = 5
+BLUE = 6
+GREEN = 13
+
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(BLUE, GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
+
+while True:
+    GPIO.output(RED, True)
+    GPIO.output(BLUE, True)
+    GPIO.output(GREEN, True)
+    time.sleep(0.5)
+    GPIO.output(RED, False)
+    GPIO.output(BLUE, False)
+    GPIO.output(GREEN, False)
+    time.sleep(0.5)
