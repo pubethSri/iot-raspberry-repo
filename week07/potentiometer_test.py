3b85f5abf6c6aa4cdd06d9dcabc1d0787c98a404
pubethSri <66070158@kmitl.ac.th>
2025-08-20 20:45:43 +0700

    add duty

--- week07/potentiometer_test.py
+++ week07/potentiometer_test.py
@@ -1,5 +1,14 @@
 import time
 import spidev
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+ledpin = 12
+
+GPIO.setup(ledpin, GPIO.OUT)
+pi_pwm = GPIO.PWM(ledpin, 1000)
+pi_pwm.start(0)
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
@@ -15,8 +24,11 @@ def ReadChannel(channel):
 while True:
     reading = ReadChannel(0)
     voltage = reading * 3.3 / 4096
-    print("Reading=%d\t Voltage=%f" %(reading, voltage)) 
-    time.sleep(0.5)
+    forduty = reading / 2.55
+    print("Reading=%d\t Voltage=%f\t Forduty=%f" %(reading, voltage, forduty))
+    pi_pwm.ChangeDutyCycle(forduty) 
+
+    time.sleep(2)
 
     
     
